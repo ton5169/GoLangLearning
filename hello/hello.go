31995a410613a4b687db1629d10aa6db6b00f2a0
Anton Irshenko <a_irshenko@wargaming.net>
2025-11-13 16:21:24 +0100

    tests

--- hello/hello.go
+++ hello/hello.go
@@ -11,14 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-    message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
-    fmt.Println(message)
+	fmt.Println(messages)
 
 }
