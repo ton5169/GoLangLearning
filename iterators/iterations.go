0a7c878233955d6ca9ae4074fccff08984abc25d
AtomicTon89 <irshenko@gmail.com>
2026-01-30 23:19:09 +0100

    iterators

---
+++ iterators/iterations.go
@@ -0,0 +1,12 @@
+package iterators
+
+import "strings"
+
+func Repeat(character string, repeatCount int) string {
+	var repeated strings.Builder
+
+	for i := 0; i < repeatCount; i++ {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
---
+++ iterators/iterations_test.go
@@ -0,0 +1,27 @@
+package iterators
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	sum := Repeat("1", 5)
+	fmt.Println(sum)
+	//Output: 11111
+}
