5d07de7ce50a7c1defa09ef2ee458950403fac52
AtomicTon89 <irshenko@gmail.com>
2026-02-05 22:00:24 +0100

    structs

--- structs/spapes.go
+++ structs/spapes.go
@@ -1,12 +1,22 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
